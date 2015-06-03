748f56303f9460ce818147b1a1b9651ce5ef22d6
Josef Wegner <josef.wegner@here.com>
2015-06-03 10:22:51 +0200

    Replaced JDK 8 class usage with JDK 6 equivalent

--- src/test/java/de/zalando/jgroups/NativeS3PingTest.java
+++ src/test/java/de/zalando/jgroups/NativeS3PingTest.java
@@ -4,8 +4,8 @@ import org.jgroups.JChannel;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class NativeS3PingTest {
@@ -16,7 +16,7 @@ public class NativeS3PingTest {
 
         final List<ClusterMember> members = new ArrayList<ClusterMember>();
         for (int n = 0; n < MEMBER_COUNT; n++) {
-            members.add(new ClusterMember("findMembersTest-" + Instant.now().getEpochSecond()));
+            members.add(new ClusterMember("findMembersTest-" + (new Date().getTime()) / 1000));
         }
 
         for (final ClusterMember member: members) {
